4cb54fa6e589aeea6bcfbf60e82b16d191119faf
Mahmoud Abdo <114188513+mahmooudabdoo@users.noreply.github.com>
2026-02-07 00:15:10 +0200

    Update LogoLink.tsx

--- src/components/shared/LogoLink.tsx
+++ src/components/shared/LogoLink.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const LogoLink = () => {
   return (
     <Link href="/" className="flex items-center gap-2 text-2xl">
-      <Image src="/sd1.png" alt="Logo" width={50} height={50} />
+      <Image src="/SD1.png" alt="Logo" width={50} height={50} />
       <span className="font-black">
   <span className="text-primary">Scan</span>
   <span className="text-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.8)]">
